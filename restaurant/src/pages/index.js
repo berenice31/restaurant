0e0f8187efa315177ec9fef8ad003d1c84956b50
berenice31 <59822075+berenice31@users.noreply.github.com>
2020-07-19 15:14:27 +0200

    burger ok

--- restaurant/src/pages/index.js
+++ restaurant/src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { FaBeer } from "react-icons/fa"
+// import { FaBeer } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
