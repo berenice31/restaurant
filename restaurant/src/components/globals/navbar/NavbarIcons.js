d6cd0486575a49400d840fc1726aa71a5521d02c
berenice31 <59822075+berenice31@users.noreply.github.com>
2020-07-20 16:07:38 +0200

    navbar over

--- restaurant/src/components/globals/navbar/NavbarIcons.js
+++ restaurant/src/components/globals/navbar/NavbarIcons.js
@@ -1,4 +1,65 @@
-import React from "react"
+import React, { Component } from "react"
+import styled from "styled-components"
+import { styles } from "../../../utils"
+import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa"
 
-const NavbarIcons = () => <div> content from NavbarIcons </div>
-export default NavbarIcons
+export default class NavbarIcons extends Component {
+  state = {
+    icons: [
+      {
+        id: 0,
+        icon: <FaFacebook className="icon facebook-icon" />,
+        path: "https://www.facebook.com",
+      },
+      {
+        id: 1,
+        icon: <FaTwitter className="icon twitter-icon" />,
+        path: "https://www.twitter.com",
+      },
+      {
+        id: 2,
+        icon: <FaInstagram className="icon instagram-icon" />,
+        path: "https://www.instagram.com",
+      },
+    ],
+  }
+  render() {
+    return (
+      <IconWrapper>
+        {this.state.icons.map(icon => {
+          return (
+            <a href={icon.path} key={icon.id} target="_blank" rel="noreferrer">
+              {" "}
+              {icon.icon}{" "}
+            </a>
+          )
+        })}
+      </IconWrapper>
+    )
+  }
+}
+const IconWrapper = styled.div`
+  .icon {
+    font-size: 1.3rem;
+    cursor: pointer;
+    ${styles.transObject({})};
+  }
+  .facebook-icon {
+    color: #3b579d;
+  }
+  .twitter-icon {
+    color: #3ab7f0;
+  }
+  .instagram-icon {
+    color: #da5f53;
+  }
+  .icon:hover {
+    color: ${styles.colors.mainYellow};
+  }
+  display: none;
+  @media (min-width: 768px) {
+    display: flex;
+    width: 10rem;
+    justify-content: space-around;
+  }
+`
